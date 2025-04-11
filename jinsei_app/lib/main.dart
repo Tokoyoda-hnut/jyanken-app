a62810505b74194f3cd95d25426dcb0cd7ad4e7f
Tokoyoda-hnut <tokoyoda.hnut@gmail.com>
2025-04-11 18:44:28 +0900

    MOD: toppageにイベントの追加まで

--- jinsei_app/lib/main.dart
+++ jinsei_app/lib/main.dart
@@ -7,45 +7,88 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const LifeCounterPage(), 
-    );
+    return MaterialApp(home: const LifeCounterPage());
   }
-}  
+}
 
 class LifeCounterPage extends StatefulWidget {
   const LifeCounterPage({super.key});
 
   @override
-  State <LifeCounterPage> createState() =>  _LifeCounterPageState();
+  State<LifeCounterPage> createState() => _LifeCounterPageState();
 }
 
-class  _LifeCounterPageState extends State <LifeCounterPage> {
- 
- Store? store;
- Box<LifeEvent>? lifeEventBox; // null許容
+class _LifeCounterPageState extends State<LifeCounterPage> {
+  Store? store;
+  Box<LifeEvent>? lifeEventBox; // null許容
+  List<LifeEvent> lifeEvents = <LifeEvent>[];
 
+  Future<void> initialize() async {
+    store = await openStore();
+    lifeEventBox = store?.box<LifeEvent>(); //storeがヌルの場合でも処理したい
+    lifeEvents = lifeEventBox?.getAll() ?? [];
+    setState(() {});
+  }
 
- Future<void> initialize() async {
-  store = await openStore();
-  lifeEventBox = store?.box<LifeEvent>(); //storeがヌルの場合でも処理したい
-  setState(() {});
- }
-
- @override
- void initState() {
-  super.initState();
-  store = openStore();
- }
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Life Counter'),
+      appBar: AppBar(title: const Text('Life Counter')),
+      body: ListView.builder(
+        itemCount: lifeEvents.length,
+        itemBuilder: (context, index) {
+          final lifeEvent = lifeEvents[index];
+          return Text(lifeEvent.title);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newLifeEvent = await Navigator.of(context).push<LifeEvent>(
+            MaterialPageRoute(
+              builder: (context) {
+                return const AddLifeEventPage();
+              },
+            ),
+          );
+          if (newLifeEvent != null) {
+            lifeEventBox?.put(newLifeEvent);
+            lifeEvents = lifeEventBox?.getAll() ?? [];
+            setState(() {});
+          }
+          print(newLifeEvent?.title);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddLifeEventPage extends StatefulWidget {
+  const AddLifeEventPage({super.key});
+
+  @override
+  State<AddLifeEventPage> createState() => _AddLifeEventPageState();
+}
+
+class _AddLifeEventPageState extends State<AddLifeEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ライフイベントを追加')),
+      body: TextFormField(
+        onFieldSubmitted: (text) {
+          final lifeEvent = LifeEvent(title: text, count: 0);
+          Navigator.of(context).pop(lifeEvent);
+        },
       ),
     );
   }
